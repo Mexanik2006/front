168e577d10497bed20543d90d989086703c15bef
Mexanik2006 <128375543+Mexanik2006@users.noreply.github.com>
2024-10-10 17:27:56 +0500

    Update Api.js
    Sss

--- src/api/Api.js
+++ src/api/Api.js
@@ -4,13 +4,13 @@ import axios from "axios";
 // const instance = axios.create({
 //     baseURL: 'http://localhost:2600'
 // });
-// const instance = axios.create({
-//    baseURL: 'https://backend-sandy-alpha.vercel.app'
-// });
+ const instance = axios.create({
+    baseURL: 'https://backend-sandy-alpha.vercel.app/'
+ });
 
-const instance = axios.create({
-    baseURL: 'https://backend-jnt4.onrender.com/' 
-} ) 
+//const instance = axios.create({
+//    baseURL: 'https://backend-jnt4.onrender.com/' 
+// } ) 
 
 export default instance
 
